87eb1f75a25746c2fad04169c2e11d839bc70f15
suy00065 <suy00065@gmail.com>
2023-01-02 22:22:09 +0900

    [feat] : json 반환값 hi!! 변경

---
+++ src/main/java/com/example/suyeonjenkinstest01/SuyeonJenkinsTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.suyeonjenkinstest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuyeonJenkinsTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuyeonJenkinsTest01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suyeonjenkinstest01/SuyeonJenkinsTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suyeonjenkinstest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuyeonJenkinsTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
